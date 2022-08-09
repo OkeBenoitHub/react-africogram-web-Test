ade51056c3ffe08b4addc8b72631ee97f1d1130c
OkeBenoitHub <okebenoithub@gmail.com>
2022-08-08 22:57:59 -0400

    1 file modified

--- public/application/views/tpls/privacy.tpl.js
+++ public/application/views/tpls/privacy.tpl.js
@@ -1,5 +1,5 @@
-import React from "react";
-import FooterBox from "./indexFoot.tpl.js";
+import React from 'react';
+import FooterBox from './indexFoot.tpl.js';
 
 export default function PrivacyTemplate() {
   return (
@@ -11,10 +11,10 @@ export default function PrivacyTemplate() {
 
 function PrivacyMain() {
   const mainContentStyle = {
-      backgroundColor: "#fff",
-      width: "100%",
-      height: "auto",
-      marginTop: "15px"
+    backgroundColor: '#fff',
+    width: '100%',
+    height: 'auto',
+    marginTop: '15px',
   };
   return (
     <div class="privacyP">
@@ -23,8 +23,8 @@ function PrivacyMain() {
           <div class="w3-row">
             <div class="w3-container">
               <div id="termsContent" style={mainContentStyle}>
-                <div class="ui basic segment" style={{ marginBottom: "15px" }}>
-                    <PrivacyPolicyText />
+                <div class="ui basic segment" style={{ marginBottom: '15px' }}>
+                  <PrivacyPolicyText />
                 </div>
               </div>
               <FooterBox />
@@ -40,27 +40,27 @@ function PrivacyPolicyText() {
   return (
     <div>
       <center>
-        <h2 style={{ color: "#4caf50" }}>Privacy Policy</h2>
+        <h2 style={{ color: '#4caf50' }}>Privacy Policy</h2>
       </center>
-      <div style={{ margin: "30px", color: "grey" }} id="privacyTpl">
+      <div style={{ margin: '30px', color: 'grey' }} id="privacyTpl">
         <p>
           Your privacy is important to us. It is Africogram&#39;s policy to
           respect your privacy and comply with any applicable law and regulation
           regarding any personal information we may collect about you, including
-          across our website,{" "}
+          across our website,{' '}
           <a href="https://www.africogram.com">https://www.africogram.com</a>,
-          and other sites we own and operate.{" "}
+          and other sites we own and operate.{' '}
         </p>
         <p>
           This policy is effective as of 14 April 2021 and was last updated on
-          14 April 2021.{" "}
+          14 April 2021.{' '}
         </p>
         <h3>Information We Collect</h3>
         <p>
           Information we collect includes both information you knowingly and
           actively provide us when using or participating in any of our services
           and promotions, and any information automatically sent by your devices
-          in the course of accessing our products and services.{" "}
+          in the course of accessing our products and services.{' '}
         </p>
         <h4>Log Data</h4>
         <p>
@@ -69,17 +69,17 @@ function PrivacyPolicyText() {
           device’s Internet Protocol (IP) address, your browser type and
           version, the pages you visit, the time and date of your visit, the
           time spent on each page, other details about your visit, and technical
-          details that occur in conjunction with any errors you may encounter.{" "}
+          details that occur in conjunction with any errors you may encounter.{' '}
         </p>
         <p>
           Please be aware that while this information may not be personally
           identifying by itself, it may be possible to combine it with other
-          data to personally identify individual persons.{" "}
+          data to personally identify individual persons.{' '}
         </p>
         <h4>Personal Information</h4>
         <p>
           We may ask for personal information which may include one or more of
-          the following:{" "}
+          the following:{' '}
         </p>
         <ul>
           <li>Name</li>
@@ -92,12 +92,12 @@ function PrivacyPolicyText() {
           We only collect and use your personal information when we have a
           legitimate reason for doing so. In which instance, we only collect
           personal information that is reasonably necessary to provide our
-          services to you.{" "}
+          services to you.{' '}
         </p>
         <h4>Collection and Use of Information</h4>
         <p>
           We may collect personal information from you when you do any of the
-          following on our website:{" "}
+          following on our website:{' '}
         </p>
         <ul>
           <li>Use a mobile device or web browser to access our content</li>
@@ -109,17 +109,17 @@ function PrivacyPolicyText() {
         <p>
           We may collect, hold, use, and disclose information for the following
           purposes, and personal information will not be further processed in a
-          manner that is incompatible with these purposes:{" "}
+          manner that is incompatible with these purposes:{' '}
         </p>
         <p>
           We may collect, hold, use, and disclose information for the following
           purposes, and personal information will not be further processed in a
-          manner that is incompatible with these purposes:{" "}
+          manner that is incompatible with these purposes:{' '}
         </p>
         <ul>
           <li>
             to enable you to customise or personalise your experience of our
-            website{" "}
+            website{' '}
           </li>
           <li>to contact and communicate with you</li>
           <li>
@@ -139,26 +139,26 @@ function PrivacyPolicyText() {
         <p>
           Please be aware that we may combine information we collect about you
           with general information or research data we receive from other
-          trusted sources.{" "}
+          trusted sources.{' '}
         </p>
         <h4>Security of Your Personal Information</h4>
         <p>
           When we collect and process personal information, and while we retain
           this information, we will protect it within commercially acceptable
           means to prevent loss and theft, as well as unauthorized access,
-          disclosure, copying, use, or modification.{" "}
+          disclosure, copying, use, or modification.{' '}
         </p>
         <p>
           Although we will do our best to protect the personal information you
           provide to us, we advise that no method of electronic transmission or
           storage is 100% secure, and no one can guarantee absolute data
           security. We will comply with laws applicable to us in respect of any
-          data breach.{" "}
+          data breach.{' '}
         </p>
         <p>
           You are responsible for selecting any password and its overall
           security strength, ensuring the security of your own information
-          within the bounds of our services.{" "}
+          within the bounds of our services.{' '}
         </p>
         <h4>How Long We Keep Your Personal Information</h4>
         <p>
@@ -166,19 +166,19 @@ function PrivacyPolicyText() {
           time period may depend on what we are using your information for, in
           accordance with this privacy policy. If your personal information is
           no longer required, we will delete it or make it anonymous by removing
-          all details that identify you.{" "}
+          all details that identify you.{' '}
         </p>
         <p>
           However, if necessary, we may retain your personal information for our
           compliance with a legal, accounting, or reporting obligation or for
           archiving purposes in the public interest, scientific, or historical
-          research purposes or statistical purposes.{" "}
+          research purposes or statistical purposes.{' '}
         </p>
         <h3>Children’s Privacy</h3>
         <p>
           We do not aim any of our products or services directly at children
           under the age of 13, and we do not knowingly collect personal
-          information about children under 13.{" "}
+          information about children under 13.{' '}
         </p>
         <h3>Disclosure of Personal Information to Third Parties</h3>
         <p>We may disclose personal information to: </p>
@@ -219,7 +219,7 @@ function PrivacyPolicyText() {
           parties in other countries: (i) we will perform those transfers in
           accordance with the requirements of applicable law; and (ii) we will
           protect the transferred personal information in accordance with this
-          privacy policy.{" "}
+          privacy policy.{' '}
         </p>
         <h3>Your Rights and Controlling Your Personal Information</h3>
         <p>
@@ -230,14 +230,14 @@ function PrivacyPolicyText() {
           personal information you understand that we will collect, hold, use
           and disclose it in accordance with this privacy policy. You retain the
           right to request details of any personal information we hold about
-          you.{" "}
+          you.{' '}
         </p>
         <p>
           If we receive personal information about you from a third party, we
           will protect it as set out in this privacy policy. If you are a third
           party providing personal information about somebody else, you
           represent and warrant that you have such person’s consent to provide
-          the personal information to us.{" "}
+          the personal information to us.{' '}
         </p>
         <p>
           If you have previously agreed to us using your personal information
@@ -245,14 +245,14 @@ function PrivacyPolicyText() {
           We will provide you with the ability to unsubscribe from our
           email-database or opt out of communications. Please be aware we may
           need to request specific information from you to help us confirm your
-          identity.{" "}
+          identity.{' '}
         </p>
         <p>
           If you believe that any information we hold about you is inaccurate,
           out of date, incomplete, irrelevant, or misleading, please contact us
           using the details provided in this privacy policy. We will take
           reasonable steps to correct any information found to be inaccurate,
-          incomplete, misleading, or out of date.{" "}
+          incomplete, misleading, or out of date.{' '}
         </p>
         <p>
           If you believe that we have breached a relevant data protection law
@@ -262,7 +262,7 @@ function PrivacyPolicyText() {
           setting out the outcome of our investigation and the steps we will
           take to deal with your complaint. You also have the right to contact a
           regulatory body or data protection authority in relation to your
-          complaint.{" "}
+          complaint.{' '}
         </p>
         <h3>Use of Cookies</h3>
         <p>
@@ -270,14 +270,14 @@ function PrivacyPolicyText() {
           activity across our site. A cookie is a small piece of data that our
           website stores on your computer, and accesses each time you visit, so
           we can understand how you use our site. This helps us serve you
-          content based on preferences you have specified.{" "}
+          content based on preferences you have specified.{' '}
         </p>
         <h3>Limits of Our Policy</h3>
         <p>
           Our website may link to external sites that are not operated by us.
           Please be aware that we have no control over the content and policies
           of those sites, and cannot accept responsibility or liability for
-          their respective privacy practices.{" "}
+          their respective privacy practices.{' '}
         </p>
         <h3>Changes to This Policy</h3>
         <p>
@@ -285,22 +285,22 @@ function PrivacyPolicyText() {
           to our business processes, current acceptable practices, or
           legislative or regulatory changes. If we decide to change this privacy
           policy, we will post the changes here at the same link by which you
-          are accessing this privacy policy.{" "}
+          are accessing this privacy policy.{' '}
         </p>
         <p>
           If required by law, we will get your permission or give you the
           opportunity to opt in to or opt out of, as applicable, any new uses of
-          your personal information.{" "}
+          your personal information.{' '}
         </p>
         <h3>Contact Us</h3>
         <p>
           For any questions or concerns regarding your privacy, you may contact
-          us using the following details:{" "}
+          us using the following details:{' '}
         </p>
         <p>
           Africogram Inc
           <br />
-          africogram.app.base@gmail.com{" "}
+          africogram.app.base@gmail.com{' '}
         </p>
       </div>
     </div>
